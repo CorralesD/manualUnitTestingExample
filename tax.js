f95fbe508808cd74d0536151784d35bfdbd6034e
Diego Corrales <diegoamcorrales@gmail.com>
2017-03-11 10:25:15 -0800

    fixed round error

--- tax.js
+++ tax.js
@@ -1,4 +1,6 @@
-function addTax(subtotal, taxRate) {
+function addTax (subtotal, taxRate) {
 	var total = subtotal * (1 + (taxRate/100));
+	var total = Math.round( total * 10) / 10;
+	console.log(typeof total);
 	return total;
 }
